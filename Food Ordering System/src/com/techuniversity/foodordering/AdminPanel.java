5c52e35c72d2ce8ce5ea5c9e6a1ea644973ce550
Nishaaawillcu <nishapoudyl@gmail.com>
2024-08-07 21:05:48 +0545

    de

--- Food Ordering System/src/com/techuniversity/foodordering/AdminPanel.java
+++ Food Ordering System/src/com/techuniversity/foodordering/AdminPanel.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class AdminPanel extends JFrame {
     public AdminPanel() {
-        setTitle("Admin Panel"); //nisha
+        setTitle("Admin Panel"); 
         setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
